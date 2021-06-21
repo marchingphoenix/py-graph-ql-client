bb2bbb51179b34217565e60a87f7b1862383bc72
marchingphoenix <eanagley@gmail.com>
2021-06-21 15:49:26 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-graph-ql-client",
+    version="0.0.1.dev0",
+    description="",
+    long_description="",
+    author="Eric Nagley",
+    packages=find_packages(exclude=["docs", "tests*"]),
+    url="https://github.com/marchingphoenix/py-graph-ql-client",
+    install_requires=[],
+)
