60f759ed0f48bafc54176f5975141460ad35750d
marchingphoenix <eanagley@gmail.com>
2021-06-22 00:02:07 -0400

    add fragment handling

--- py_graph_ql_client/Query.py
+++ py_graph_ql_client/Query.py
@@ -1,9 +1,26 @@
 from __future__ import annotations
 
-from typing import List, Optional, Tuple
+from typing import Generator, List, Optional, Tuple, Union
+from abc import ABC, abstractmethod
+from itertools import chain
 
 
-class Query:
+class GQLObject(ABC):
+    name = str()
+    variables = None
+
+    @abstractmethod
+    def generate_query(self):
+        raise NotImplementedError
+
+    def args_list_to_query(self) -> str:
+        if self.variables:
+            return ", ".join([f"{k}: {v}" for k, v in self.variables.items()])
+        else:
+            return str()
+
+
+class Query(GQLObject):
     def __init__(self, name: str, variables: Optional[dict] = None) -> None:
         "Initialize query Constructor"
         self.name = name
@@ -11,12 +28,14 @@ class Query:
         self._resources: List[Resource] = list()
 
     def generate_query(self) -> str:
-        if self.variables:
-            args = [f"{k}: {v}" for k, v in self.variables.items()]
-        else:
-            args = []
-        resources = [x.generate_query() for x in self.resources]
-        query = f"query {self.name}({', '.join(args)}){''.join(resources)}"
+        fragments = list()
+        resources = list()
+        for resource in self.resources:
+            resources.append(resource.generate_query())
+            fragments.extend(resource.get_fragment_queries())
+        query = f"query {self.name}({self.args_list_to_query()}){''.join(resources)}"
+        if fragments:
+            query = f"{' '.join(fragments)} {query}"
         return query
 
     def validate_fragments(self):
@@ -30,12 +49,17 @@ class Query:
         self._resources.append(resource)
 
 
-class Fragment:
-    def __init__(self, name) -> None:
+class Fragment(GQLObject):
+    def __init__(self, name: str, resource: Resource) -> None:
         self.name = name
+        self.resource = resource
+
+    def generate_query(self) -> str:
+        query = f"fragment {self.name} on {self.resource.name} {{{self.resource.properties_to_string()}}}"
+        return query
 
 
-class Resource:
+class Resource(GQLObject):
     def __init__(
         self,
         name: str,
@@ -43,41 +67,53 @@ class Resource:
         sub_resource: bool = False,
         page_info: bool = False,
         cursor: bool = False,
-        name_override: Optional[str] = None
+        name_override: Optional[str] = None,
     ) -> None:
         self.name = name
         self.variables = variables
-        self._properties: List[str, Tuple[str, Fragment], Resource] = list()
+        self._properties: List[Union[str, Resource]] = list()
         self.is_sub_resource = sub_resource
         self.page_info = page_info
         self.cursor = cursor
         self.name_override = name_override
+        self._fragments: List[Tuple[str, Fragment]] = list()
 
     @property
     def properties(self):
-        return self._properties
+        for property in chain(self._properties, self._fragments):
+            yield property
+
+    def get_fragment_queries(self):
+        if self._fragments:
+            queries = [x[1].generate_query() for x in self._fragments]
+            return queries
+        else:
+            return list()
 
     def add_simple_properties(self, properties: List[str]):
         self._properties.extend(properties)
 
     def add_sub_resource(self, resource: Resource):
-        if resource.is_sub_resource:
-            self._properties.append(resource)
-        else:
-            # TODO: Some sort of Error Handling
-            pass
+        self._properties.append(resource)
 
     def add_fragment(self, property: str, fragment: Fragment):
-        self._properties.append((property, fragment))
+        self._fragments.append((property, fragment))
 
-    def generate_query(self):
+    def properties_to_string(self) -> str:
+        properties = list()
+        for property in self.properties:
+            if isinstance(property, Resource):
+                properties.append(property.generate_query())
+            if isinstance(property, tuple):
+                properties.append(f"{property[0]} {{...{property[1].name}}}")
+            else:
+                properties.append(f"{property}")
+        return " ".join(properties)
+
+    def generate_query(self) -> str:
         if self.is_sub_resource:
             pass
         else:
-            if self.variables:
-                args = [f"{k}: {v}" for k, v in self.variables.items()]
-            else:
-                args = []
             if self.page_info:
                 page_info = "{pageInfo{hasNextPage matchCount} "
             else:
@@ -90,14 +126,6 @@ class Resource:
                 nodes = f"{self.name_override}: "
             else:
                 nodes = ""
-            properties = list()
-            for property in self.properties:
-                if isinstance(property, Resource):
-                    properties.append(property.generate_query())
-                if isinstance(property, tuple):
-                    properties.append(f"{property[0]}: ...{property[1].name}")
-                else:
-                    properties.append(f"{property}")
-            query = f"{{{self.name}({', '.join(args)}){{{page_info}edges{{{cursor}{nodes}{{{' '.join(properties)}}}}}}}}}"
+            query = f"{{{self.name}({self.args_list_to_query()}){{{page_info}edges{{{cursor}{nodes}{{{self.properties_to_string()}}}}}}}}}"
 
             return query
--- tests/development_test.py
+++ tests/development_test.py
@@ -1,10 +1,14 @@
-from py_graph_ql_client.Query import Query, Resource
+from py_graph_ql_client.Query import Fragment, Query, Resource
 
 query = Query('DeviceFetch', {'$search': 'DeviceSearch', '$order': '[ConnectionOrder]', '$first': 'Int'})
 
-resource = Resource('devices', {'first': '$first', 'search': '$search', 'after': '""', "order": "$order"}, page_info=True, cursor=True, name_override="device")
-resource.add_simple_properties(['id', 'name'])
+asset = Resource('asset', sub_resource=True)
+asset.add_simple_properties(['id', 'name'])
+fragment = Fragment('asset', asset)
+devices = Resource('devices', {'first': '$first', 'search': '$search', 'after': '""', "order": "$order"}, page_info=True, cursor=True, name_override="device")
+devices.add_simple_properties(['id', 'name'])
+devices.add_fragment('asset', fragment)
 
-query.add_resource(resource)
+query.add_resource(devices)
 
 print(query.generate_query())
